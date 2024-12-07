7657a554e2f7612458eae640271f0736a83203c8
rubenmongon <rubenmongon@correo.ugr.es>
2024-12-07 11:14:48 +0100

    pantallas de tareas plantillas y arreglar algunas cosas

--- dali/lib/main.dart
+++ dali/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:dali/screen/adminAsignarTareas.dart';
+import 'package:dali/screen/adminCreadorTareas.dart';
+import 'package:dali/screen/adminTareasPlantilla.dart';
 import 'package:dali/screen/adminUsuarios.dart';
 import 'package:dali/screen/splashScreen.dart';
 import 'package:flutter/material.dart';
@@ -19,10 +21,14 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFF44034)),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      // home: const SplashScreen(),
+      // home: AdminCreadorTareas(),
+      home: AdminTareasPlantilla(),
       routes: {
         '/adminUsuarios': (context) => AdminUsuarios(),
+        '/adminCreadorTareas': (context) => AdminTareasPlantilla(),
         '/adminAsignarTarea': (context) => AdminAsignarTareas(),
+
       },
       debugShowCheckedModeBanner: false,
     );
